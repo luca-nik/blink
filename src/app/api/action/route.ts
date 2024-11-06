a01bbc2336230fd6bfa4657eed6c2a1a9661213b
luca-nik <luca.nicoli.engineer@gmail.com>
2024-11-06 16:35:24 +0100

    proceeding

--- src/app/api/action/route.ts
+++ src/app/api/action/route.ts
@@ -1,7 +1,8 @@
-import { ActionGetResponse, ActionPostResponse } from "@solana/actions"
+import { ActionGetResponse, ActionPostResponse, ActionPostRequest, ACTIONS_CORS_HEADERS } from "@solana/actions"
+import {PublicKey, SystemProgram, Transaction} from "@solana/web3.js"
 
 export async function GET(request: Request) {
-  const response: ActionGetResponse = {
+  const responseBody: ActionGetResponse = {
     icon: "https://naiky.vercel.app/_images/luca-nik.jpeg",
     description: "My demo blink",
     title: "BLINK",
@@ -11,18 +12,32 @@ export async function GET(request: Request) {
     },
 
   }
-  return Response.json(response)
+  const response = Response.json(responseBody, {headers: ACTIONS_CORS_HEADERS});
+  return response
 }
 
 
 export async function POST(request: Request) {
   
-  const postRequest = request.json();
+  const requestBody: ActionPostRequest = await request.json();
+  const userPubkey = requestBody.account;
+  console.log(userPubkey);
+
+  const tx = new Transaction();
+  tx.feePayer = new PublicKey(userPubkey);
+  tx.recentBlockhash = SystemProgram.programId.toBase58(); // you should put the recent blockhash
+  const serialTX =  tx.serialize({requireAllSignatures: false, verifySignatures: false}).toString("base64");
 
   const response: ActionPostResponse = {
-    transaction: "",
-    message: "Not implemented yet"
+    transaction: serialTX,
+    message: "hello " + userPubkey,
   };
-  return Response.json(response)
 
+
+  return Response.json(response, {headers: ACTIONS_CORS_HEADERS})
+
+}
+
+export async function OPTIONS(request: Request){
+  return new Response(null, {headers: ACTIONS_CORS_HEADERS})
 }
