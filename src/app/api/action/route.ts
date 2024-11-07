c188038d6f5fbc19b80f9268ce0be47fc1d6ab55
luca-nik <luca.nicoli.engineer@gmail.com>
2024-11-07 13:51:42 +0100

    blink

--- src/app/api/action/route.ts
+++ src/app/api/action/route.ts
@@ -1,14 +1,35 @@
 import { ActionGetResponse, ActionPostResponse, ActionPostRequest, ACTIONS_CORS_HEADERS } from "@solana/actions"
-import {PublicKey, SystemProgram, Transaction} from "@solana/web3.js"
+import {clusterApiUrl, PublicKey, SystemProgram, Transaction, Connection} from "@solana/web3.js"
+import { stringify } from "querystring";
 
 export async function GET(request: Request) {
   const responseBody: ActionGetResponse = {
     icon: "https://naiky.vercel.app/_images/luca-nik.jpeg",
-    description: "My demo blink",
-    title: "BLINK",
-    label: "Click me",
+    description: "A demo blink to donate to cypherscientist.",
+    title: "cypherscientist",
+    label: "Donate",
     error: {
-      message: "Not implemented yet!"
+      message: "Huston we got a problem"
+    },
+    links: {
+      actions: [
+        {
+          href: request.url,
+          label: "same action",
+        },
+
+        {
+          href: request.url+"?action=nickname&name={nameParam}",
+          label: "nickname",
+          parameters:[
+            {
+              name: "nameParam",
+              label: "nickname",
+              required: true,
+            }
+          ]
+        }
+    ]
     },
 
   }
@@ -23,16 +44,38 @@ export async function POST(request: Request) {
   const userPubkey = requestBody.account;
   console.log(userPubkey);
 
+  const url = new URL(request.url);
+  const action = url.searchParams.get('action');
+  const param = url.searchParams.get('name');
+
+  const user = new PublicKey(userPubkey)
+
+  const connection = new Connection(clusterApiUrl("devnet"));
+
+
+  let message_:string;
+  if (action =="nickname"){
+    const name_ = param!;
+    message_ = "Hey " + name_;
+  }else{
+    message_ = "Thanks for your donation, this will help DeSci.";
+  };
+
   const tx = new Transaction();
+  const ix = SystemProgram.transfer({
+    fromPubkey: user,
+    toPubkey: new PublicKey('4vQWXAFDZtpQ4o5AmJ1bdDZ8UDFD6tSnqnMqskWB6sXu'),
+    lamports: 1,
+  });
+  tx.add(ix);
   tx.feePayer = new PublicKey(userPubkey);
-  tx.recentBlockhash = SystemProgram.programId.toBase58(); // you should put the recent blockhash
+  tx.recentBlockhash = (await connection.getLatestBlockhash({commitment: "finalized"})).blockhash;
   const serialTX =  tx.serialize({requireAllSignatures: false, verifySignatures: false}).toString("base64");
 
   const response: ActionPostResponse = {
     transaction: serialTX,
-    message: "hello " + userPubkey,
+    message: message_,
   };
-
 
   return Response.json(response, {headers: ACTIONS_CORS_HEADERS})
 
