eba9cfc15d4b217b175e8bd37075c95851616189
luca-nik <luca.nicoli.engineer@gmail.com>
2024-11-07 16:02:39 +0100

    Dr

--- src/app/api/action/route.ts
+++ src/app/api/action/route.ts
@@ -1,12 +1,13 @@
 import { ActionGetResponse, ActionPostResponse, ActionPostRequest, ACTIONS_CORS_HEADERS } from "@solana/actions"
-import {clusterApiUrl, PublicKey, SystemProgram, Transaction, Connection} from "@solana/web3.js"
+import {clusterApiUrl, PublicKey, SystemProgram, Transaction, Connection, LAMPORTS_PER_SOL} from "@solana/web3.js"
+import { headers } from "next/headers";
 import { stringify } from "querystring";
 
 export async function GET(request: Request) {
   const responseBody: ActionGetResponse = {
-    icon: "https://naiky.vercel.app/_images/luca-nik.jpeg",
-    description: "A demo blink to donate to cypherscientist.",
-    title: "cypherscientist",
+    icon: "http://localhost:3000/Dr.Cypher.png",
+    description: "Let's empower supporters to fund scientists directly in real-time using blockchain technology.\n\nThis is a Blink (Blockchain Link) and allows you to directly and safely transfer funds from your wallet to Dr. CatBlock's to help her in her quest towards decentralization of science.\n\nImagine this being you, allowing your fellow scientists and organization to finance your work directly, speeding up the pace of scientific progress.",
+    title: "Donate to Dr. CatBlock, the paladin of DeSci.",
     label: "Donate",
     error: {
       message: "Huston we got a problem"
@@ -14,17 +15,12 @@ export async function GET(request: Request) {
     links: {
       actions: [
         {
-          href: request.url,
-          label: "same action",
-        },
-
-        {
-          href: request.url+"?action=nickname&name={nameParam}",
-          label: "nickname",
+          href: request.url+"?action=donate&amount={amountParam}",
+          label: "donate",
           parameters:[
             {
-              name: "nameParam",
-              label: "nickname",
+              name: "amountParam",
+              label: "Amount in SOL",
               required: true,
             }
           ]
@@ -46,7 +42,7 @@ export async function POST(request: Request) {
 
   const url = new URL(request.url);
   const action = url.searchParams.get('action');
-  const param = url.searchParams.get('name');
+  const amount_ = url.searchParams.get('amount');
 
   const user = new PublicKey(userPubkey)
 
@@ -54,18 +50,24 @@ export async function POST(request: Request) {
 
 
   let message_:string;
-  if (action =="nickname"){
-    const name_ = param!;
-    message_ = "Hey " + name_;
+  // Convert amount_ to a number and handle cases where it's null or invalid
+  const amount_lamports = amount_ ? parseFloat(amount_) * LAMPORTS_PER_SOL : 0;
+
+  if (isNaN(amount_lamports)) {
+    return Response.json({ error: "Invalid amount" }, { status: 400 , headers: ACTIONS_CORS_HEADERS});
+  }
+
+  if (action =="donate"){
+    message_ = "Thanks for your donation of " + amount_ + " SOL, this will help DeSci."; 
   }else{
-    message_ = "Thanks for your donation, this will help DeSci.";
+    message_ ="Thanks for your donation, this will help DeSci."; 
   };
 
   const tx = new Transaction();
   const ix = SystemProgram.transfer({
     fromPubkey: user,
     toPubkey: new PublicKey('4vQWXAFDZtpQ4o5AmJ1bdDZ8UDFD6tSnqnMqskWB6sXu'),
-    lamports: 1,
+    lamports: amount_lamports,
   });
   tx.add(ix);
   tx.feePayer = new PublicKey(userPubkey);
