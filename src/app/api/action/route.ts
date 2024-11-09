60df76796a1e2f78448b0c540935bb8a940d4fa7
luca-nik <luca.nicoli.engineer@gmail.com>
2024-11-09 15:49:24 +0000

    bee

--- src/app/api/action/route.ts
+++ src/app/api/action/route.ts
@@ -1,36 +1,50 @@
-import { ActionGetResponse, ActionPostResponse, ActionPostRequest, ACTIONS_CORS_HEADERS } from "@solana/actions"
+import { ActionGetResponse, ActionPostResponse, ActionPostRequest, 
+  ACTIONS_CORS_HEADERS, createActionHeaders, ActionError } from "@solana/actions"
 import {clusterApiUrl, PublicKey, SystemProgram, Transaction, Connection, LAMPORTS_PER_SOL} from "@solana/web3.js"
 import { headers } from "next/headers";
 import { stringify } from "querystring";
 
-export async function GET(request: Request) {
-  const responseBody: ActionGetResponse = {
-    icon: "http://localhost:3000/Dr.Cypher.png",
-    description: "Let's empower supporters to fund scientists directly in real-time using blockchain technology.\n\nThis is a Blink (Blockchain Link) and allows you to directly and safely transfer funds from your wallet to Dr. CatBlock's to help her in her quest towards decentralization of science.\n\nImagine this being you, allowing your fellow scientists and organization to finance your work directly, speeding up the pace of scientific progress.",
-    title: "Donate to Dr. CatBlock, the paladin of DeSci.",
-    label: "Donate",
-    error: {
-      message: "Huston we got a problem"
-    },
-    links: {
-      actions: [
-        {
-          href: request.url+"?action=donate&amount={amountParam}",
-          label: "donate",
-          parameters:[
-            {
-              name: "amountParam",
-              label: "Amount in SOL",
-              required: true,
-            }
-          ]
-        }
-    ]
-    },
+// create the standard headers for this route (including CORS)
+const cors_headers = createActionHeaders(); //Da aggiustare
 
+export async function GET(request: Request) {
+  try {
+    const responseBody: ActionGetResponse = {
+      icon: "http://localhost:3000/CypherBee.jpg",
+      description: "Read Prof. CypherBee's [DeSci whitepaper](http://localhost:3000/DeSci.pdf) and join the vision of a open, democratic, and decentralized science using blockchain technology.\n\nThis is a Blink (Blockchain Link) and allows you to directly and safely transfer funds from your wallet to Prof. CypherBee's to help her in her quest towards decentralization of science.\n\nImagine this being you, allowing your fellow scientists and organization to finance your work directly, speeding up the pace of scientific progress.",
+      title: "Imagine a world where scientists earn fairly for their contributions. Support the cypherbees and join the colony!",
+      label: "Donate",
+      error: {
+        message: "Huston we got a problem"
+      },
+      links: {
+        actions: [
+          {
+            href: request.url+"?action=donate&amount={amountParam}",
+            label: "donate",
+            parameters:[
+              {
+                name: "amountParam",
+                label: "Amount in SOL",
+                required: true,
+              }
+            ]
+          }
+      ]
+      },
+
+    }
+    const response = Response.json(responseBody, {headers: ACTIONS_CORS_HEADERS});
+    return response;
+  } catch(err) {
+    console.log(err);
+    let actionError: ActionError = { message: "An unknown error occurred" };
+    if (typeof err == "string") actionError.message = err;
+    return Response.json(actionError, {
+      status: 400,
+      headers: ACTIONS_CORS_HEADERS,
+    });
   }
-  const response = Response.json(responseBody, {headers: ACTIONS_CORS_HEADERS});
-  return response
 }
 
 
